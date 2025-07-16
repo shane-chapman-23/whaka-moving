f99cbf586c6e43410e746aadef57f3af03ca48b0
shane-chapman-23 <chapmanshane23@gmail.com>
2025-07-16 23:31:39 +1200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar";
+
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./NavBar.module.css";
+
+export default function NavBar() {
+  return <div>NAV BAR</div>;
+}
